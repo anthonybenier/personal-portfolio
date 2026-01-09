ce69c19af063865ecf3a465a5c874919d0ebf4cb
Anthony Benier <anthony.benier@outlook.fr>
2026-01-09 16:39:27 +0100

    feat(perf): add vercel speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { lazy, Suspense } from "react";
 const Home = lazy(() => import("./pages/Home").then(module => ({ default: module.Home })));
 const ProjectDetails = lazy(() => import("./pages/ProjectDetails").then(module => ({ default: module.ProjectDetails })));
 
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 function App() {
   return (
     <Router>
@@ -13,6 +15,7 @@ function App() {
           <Route path="/project/:id" element={<ProjectDetails />} />
         </Routes>
       </Suspense>
+      <SpeedInsights />
     </Router>
   );
 }
